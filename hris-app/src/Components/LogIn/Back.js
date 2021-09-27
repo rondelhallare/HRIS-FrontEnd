d4e0d902aaccc8690c0823604d32fd17bc336a28
rondelhallare <hallarerondel@yahoo.com>
2021-09-27 17:46:31 +0800

    finalize layout of log in page

--- hris-app/src/Components/LogIn/Back.js
+++ hris-app/src/Components/LogIn/Back.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Box } from '@mui/system';
 import Header from './Header';
 import Form from './Form';
-import Footer from '../footer/Footer'
 
 function Back() {
     return (
@@ -13,7 +12,7 @@ function Back() {
                 textAlign: 'center',
                 justifyContent: 'center',
                 bgcolor: 'secondary.main',
-                minHeight: '95vh',
+                minHeight: '100vh',
                 width: 'auto',
             }}
             >
@@ -29,16 +28,6 @@ function Back() {
                     <Form />
                 </Box>
             </Box >
-
-            <Box sx =  {{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                bgcolor: 'primary.main',
-                height: '10vh',
-            }}>
-                <Footer />
-            </Box>
         </Box>
 
 
