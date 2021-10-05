5d5fd62c9f087f46fdcdbceee63a4601867d5321
rondelhallare <hallarerondel@yahoo.com>
2021-10-05 13:03:33 +0800

    fix folder structure

---
+++ src/Components/Dashboard/Body.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box } from '@mui/system';
+import { Typography } from '@mui/material';
+import NavBar from './NavBar';
+
+
+function Body() {
+    return (
+        <NavBar />
+    )
+}
+
+export default Body
---
+++ src/Components/Dashboard/NavBar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Drawer } from '@mui/material'
+import { Accordion } from '@mui/material'
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Typography from '@mui/material/Typography';
+import { Box } from '@mui/system';
+
+
+const NavBar = () => {
+    return (
+        <Drawer>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography>Accordion 1</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+
+            </Accordion>
+        </Drawer>
+    )
+}
+
+export default NavBar
