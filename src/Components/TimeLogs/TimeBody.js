9daae8ecece280fb36d78360e3f7aaba5005de08
rondelhallare <hallarerondel@yahoo.com>
2021-10-28 20:47:21 +0800

    Fix layout and design in Time Logs

--- src/Components/TimeLogs/TimeBody.js
+++ src/Components/TimeLogs/TimeBody.js
@@ -1,12 +1,45 @@
 import React from 'react'
 import { Box } from '@mui/system';
 import { Switch, Route, Link, BrowserRouter as router } from 'react-router-dom'
+import Section from './Section';
+import TimeLogsTitle from './TimeLogsTitle';
 
 function TimeBody() {
     return (
-       <Box>
+        <Box>
 
-       </Box>
+            {/* Main Body */}
+            <Box sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+                <TimeLogsTitle />
+            </Box>
+
+            {/* Background */}
+            <Box sx={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                bgcolor: 'pengu.main',
+                boxShadow: 5,
+                borderRadius: '10px',
+                height: '80vh',
+                width: '80%',
+                p: '0 3%',
+                m: '0 10%',
+                
+            }}>
+                
+                {/* Section Components */}
+                <Box sx = {{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}>
+                    <Section />
+                </Box>
+            </Box>
+        </Box>
     )
 }
 
